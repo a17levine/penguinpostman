d1f3ee4487e8999e29b97212717f296eae0c0bbf
Eric Levine <djlevine@gmail.com>
2013-07-13 17:37:42 -0500

    Renamed view

--- app/models/mailing.rb
+++ app/models/mailing.rb
@@ -1,8 +1,10 @@
 class Mailing < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :pages, :file_path, :user_email, :user_first, :user_last, :user_street_1, 
+  :user_street_2, :user_city, :user_state, :user_zip, :recipient_first, :recipient_last,
+  :recipient_street_1, :recipient_street_2, :recipient_city, :recipient_state, :recipient_zip
 
   validates :user_email, presence: true
 
-  mount_uploader :file_path, 
+  mount_uploader :file_path, ImageUploader
 
 end
