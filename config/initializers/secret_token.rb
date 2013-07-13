3a8f96a42ff35316c8793570bc4b06f0fc250d9d
Alex Levine <a17levine@gmail.com>
2013-07-13 12:11:10 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Penguinpostman::Application.config.secret_token = '35c3c73655893488b1c2f17bfc1d2691bafecb991ed1854fa40aee98eea020200d3abb5e1553eef324329a2ba95527ef1f2b964755d4281167503b584ae1e5a0'
