b95774b44b8044e49e9f629b44793fa303be0910
Alex Levine <a17levine@gmail.com>
2013-07-14 12:18:55 -0500

    finished correct logic for payments and error handling

--- app/controllers/mailings_controller.rb
+++ app/controllers/mailings_controller.rb
@@ -5,12 +5,14 @@ class MailingsController < ApplicationController
   	@mailing = Mailing.new
   end
 
+  # if our create action renders or redirects, do that
+  # otherwise, default to "create" view (the view is the name of action)
+  # finally, throw an error, we give up
   def create
   	@mailing = Mailing.new(params[:mailing])
 
     if @mailing.save
-      flash[:notice] = "Database record successfully created"
-      render action: 'edit'
+      redirect_to edit_mailing_url(@mailing), notice: "Database record successfully created"
     else
       render action: 'new'
       flash[:notice] = "There were some errors"
@@ -32,10 +34,13 @@ class MailingsController < ApplicationController
           :description => "Charge for test@example.com"
         )
         flash[:notice] = "Thank you for your order! You should expect it to arrive within 4-6 business days!"
+        @mailing.order_processed = true
+        @mailing.save
         redirect_to root_path
       rescue Stripe::CardError => e
-        flash[:error] = e.message
-        redirect_to edit_mailing_path(@mailing.id)
+        # flash[:error] = e.message
+        # redirect_to edit_mailing_path(@mailing.id)
+        redirect_to edit_mailing_path(@mailing.id), :notice => "#{e.message}"
       end
     end
   end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,8 @@
 Rails.configuration.stripe = {
   # :publishable_key => ENV['PUBLISHABLE_KEY'],
   # :secret_key      => ENV['SECRET_KEY']
-  :publishable_key => 'pk_live_G3naCbjf6XlAsRd2SNILUYfe',
-  :secret_key      => 'sk_live_bqhWyhPYo38yaNFEYgX1wuV8'
+  :publishable_key => 'pk_test_hUrf8D6B8e8qtDHEBm9VVOuP',
+  :secret_key      => 'sk_test_YhAnEXdbigIFk6vhtFo3XHhc'
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
