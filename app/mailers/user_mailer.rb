97bfb5dedf93b4079a6ce1db1dbf783ad68850b3
Eric Levine <djlevine@gmail.com>
2013-08-01 00:50:57 -0500

    Added mailer for mailing confirmation

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,17 +1,17 @@
 class UserMailer < ActionMailer::Base
+  default from: "auctioneerbot@gmail.com"
 
   def order_confirmation_email(mailing)
-    mail(:to => mailing.user_email, 
-         :from => "auctioneerbot@gmail.com",
-         :subject => "CONFIRMATION - Thank You For Your Purchase")
+    @mailing = mailing
+    mail(to:  @mailing.user_email, 
+         subject: "CONFIRMATION - Thank You For Your Purchase")
   end
 
   def order_submit(mailing)
     @mailing = mailing
     attachments["#{@mailing.file_path.path.split('/').last}"] = File.read("/uploads/mailing/file_path/#{@user.id}/#{@mailing.file_path.path.split('/').last}")
-    mail(:to => @user.user_email, 
-         :from => "auctioneerbot@gmail.com",
-         :subject => "INCOMING ORDER #{@mailing.id}")
+    mail(to: @user.user_email,
+         subject: "INCOMING ORDER #{@mailing.id}")
     attachments.deliver
   end
 end
